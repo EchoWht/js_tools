667292378bfc31fdf93ac7a8fcf8430f07d76fe4
blskye <echowht@qq.com>
2018-08-27 15:41:32 +0800

    公共函数

--- tools.js
+++ tools.js
@@ -86,3 +86,48 @@ function changeUrlArg(url, arg, val) {
     return url.match(pattern) ? url.replace(eval('/(' + arg + '=)([^&]*)/gi'), replaceText) : (url.match('[\?]') ? url +
         '&' + replaceText : url + '?' + replaceText);
 }
+
+/**
+ * 将换行的字符串转为数组
+ * @param text
+ * @returns {Array}
+ */
+function textValToArr(text) {
+    var arr = text.split("\n");
+    for(var i=0;i<arr.length;i++){
+        arr[i]=removeAllSpace(arr[i]);
+    }
+    return arr;
+}
+/**
+ * 去除字符串所有空格
+ * @param str
+ * @returns {void|XML|string}
+ */
+function removeAllSpace(str) {
+    return str.replace(/\s+/g, "");
+}
+
+/**
+ * 根据元素id选中
+ * @param element
+ */
+function selectElement(element) {
+    var text = document.getElementById(element);
+    if (document.body.createTextRange) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        /*if(selection.setBaseAndExtent){
+            selection.setBaseAndExtent(text, 0, text, 1);
+        }*/
+    } else {
+        alert("none");
+    }
+}
